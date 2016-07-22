7f2b8160a990f6d79c4027eb23398fa9c4feca0f
Narendra Rajput <narendra@petasense.com>
2016-07-22 08:24:18 +0530

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/auth/failure', to: 'sessions#error', as: 'failure'
   get '/profile', to: 'sessions#show', as: 'show'
   get '/signout', to: 'sessions#destroy', as: 'signout'
+  get '/wishlists', to: 'wishlists#show', as: 'show_wishlist'
 end
